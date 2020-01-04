48eecf7427fc5060f031495d4675530a5e07a71d
TsuyoshiOiwa <42800239+TsuyoshiOiwa@users.noreply.github.com>
2020-01-04 22:10:07 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ GITHUB_EMAIL=$2
 GITHUB_TOKEN=$3
 COMMIT_MESSAGE=$4
 
-echo $1
-echo $2
-
 WORKDIR=/${GITHUB_REPOSITORY}
 git clone -b master https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY} $WORKDIR
 #git remote set-url origin https://${GITHUB_NAME}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
@@ -25,7 +22,7 @@ result=$?
 if [$result != "0"]; then
     exit $result
 fi
-ls -al
+git config -l
 echo ::git add
 git add docs
 echo ::git commit
