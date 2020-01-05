43306f0d1cd0b9d11824ff8a0b24c12e27a1e4de
TsuyoshiOiwa <42800239+TsuyoshiOiwa@users.noreply.github.com>
2020-01-05 10:44:50 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,21 @@ GITHUB_TOKEN=$3
 COMMIT_MESSAGE=$4
 
 WORKDIR=/${GITHUB_REPOSITORY}
-git clone -b master https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY} $WORKDIR
+#git clone -b master https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY} $WORKDIR
 #git remote set-url origin https://${GITHUB_NAME}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
-#git checkout -b master
+git checkout -b master
 
 git config --global user.name "${GITHUB_NAME}"
 git config --global user.email "${GITHUB_EMAIL}"
 
 cd $WORKDIR
+ls -al
 
 echo ::build-asciidoc
 ./gradlew clean
 ./gradlew asciidoc
 result=$?
-
+ls -al
 if [$result != "0"]; then
     exit $result
 fi
